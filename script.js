2c3f2debd9980a02c67a64ba69494790fe4a2931
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-04-15 08:37:22 -0300

    ff

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function (){
+
+const botaoacessibilidade = document.grtElementbyid('botao-acesssibilidade');
+const opcoesDeAcesibilidade = document.grtElementByid (opcoes-acessibilidade);
+
+botaoDeAcesibilidade.addEventListener ('click', function(){
+botaoDeAcesibilidade.classList.toggle('rotacao-botao');
+opcoesDeAcesibilidade.classList.toggle('apresenta-list')
+})
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
